ce5365573e21bc156aaf6fcdb1f50583af1e6b52
diegoruiz <diegoruiz@LAB3-6>
2026-03-16 09:54:18 -0600

    final

--- parcial1/Form1.Designer.cs
+++ parcial1/Form1.Designer.cs
@@ -33,6 +33,12 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
             this.horaCita = new System.Windows.Forms.ComboBox();
+            this.mostrar = new System.Windows.Forms.ListBox();
+            this.Itemas = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // doctorList
@@ -42,6 +48,7 @@
             this.doctorList.Name = "doctorList";
             this.doctorList.Size = new System.Drawing.Size(121, 21);
             this.doctorList.TabIndex = 0;
+            this.doctorList.Text = "Doctor";
             this.doctorList.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // pacientesList
@@ -51,6 +58,7 @@
             this.pacientesList.Name = "pacientesList";
             this.pacientesList.Size = new System.Drawing.Size(121, 21);
             this.pacientesList.TabIndex = 1;
+            this.pacientesList.Text = "Paciente";
             // 
             // dateTimePicker1
             // 
@@ -61,7 +69,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(143, 298);
+            this.button1.Location = new System.Drawing.Point(118, 298);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
@@ -77,16 +85,80 @@
             "13:00 hrs",
             "15:00 hrs",
             "17:00 hrs"});
-            this.horaCita.Location = new System.Drawing.Point(111, 247);
+            this.horaCita.Location = new System.Drawing.Point(97, 247);
             this.horaCita.Name = "horaCita";
             this.horaCita.Size = new System.Drawing.Size(121, 21);
             this.horaCita.TabIndex = 4;
+            this.horaCita.Text = "Hora";
+            // 
+            // mostrar
+            // 
+            this.mostrar.FormattingEnabled = true;
+            this.mostrar.Location = new System.Drawing.Point(310, 59);
+            this.mostrar.Name = "mostrar";
+            this.mostrar.Size = new System.Drawing.Size(478, 277);
+            this.mostrar.TabIndex = 5;
+            // 
+            // Itemas
+            // 
+            this.Itemas.FormattingEnabled = true;
+            this.Itemas.Location = new System.Drawing.Point(60, 379);
+            this.Itemas.Name = "Itemas";
+            this.Itemas.Size = new System.Drawing.Size(121, 21);
+            this.Itemas.TabIndex = 6;
+            this.Itemas.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(351, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Cantidad de Citas: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(310, 368);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 53);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Mostrar Citas";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(494, 368);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 53);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Ordenar por fecha";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(643, 368);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 53);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Ordenado pr Doctor";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Itemas);
+            this.Controls.Add(this.mostrar);
             this.Controls.Add(this.horaCita);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePicker1);
@@ -95,6 +167,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -105,6 +178,12 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox horaCita;
+        private System.Windows.Forms.ListBox mostrar;
+        private System.Windows.Forms.ComboBox Itemas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- parcial1/Form1.cs
+++ parcial1/Form1.cs
@@ -51,28 +51,55 @@ namespace parcial1
             reader.Close();
         }
 
+        private void leerCitas()
+        {
+            string nombreArchivo = @"C:\Users\diegoruiz\source\repos\parcial1\parcial1\bin\Debug\Citas.txt";
+            FileStream stream = new FileStream(nombreArchivo, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            while (reader.Peek() > -1)
+            {
+                string linea = reader.ReadLine();
+                mostrar.Items.Add(linea);
+                Itemas.Items.Add(linea);
+
+                int cant = Itemas.Items.Count;
+                label1.Text = "Cantidad de Citas:  " + cant;
+            }
+
+    
+            reader.Close();
+        }
+
         private void GuardarCita(string nombreArchivo)
         {
             FileStream stream = new FileStream(nombreArchivo, FileMode.Append, FileAccess.Write);
             StreamWriter writer = new StreamWriter(stream);
             // foreach (var nombre in leerAlumnos)
             {
-                //String fechahora = dateTimePicker1.Select;
-               // String dpi = alumno.Substring(0, 13);
+                String paciente = pacientesList.SelectedItem.ToString();
+                String nombrePaciente = paciente.Substring(14, 9);
 
-                
+                String doctor = doctorList.SelectedItem.ToString();
+                String nombreDoctor = doctor.Substring(5, 13);
+                String especialidad = doctor.Substring(19);
 
-                writer.Write(pacientesList.SelectedItem);
-                writer.Write(" ");
-                writer.Write(doctorList.SelectedItem);
-                writer.Write(" ");
-                writer.Write(dateTimePicker1.Value.ToShortDateString());
-                writer.Write(" ");
-                writer.WriteLine(horaCita.SelectedItem);
+                String fecha = dateTimePicker1.Value.ToShortDateString();
+                String hora = horaCita.SelectedItem.ToString();
 
-                // inscripciones.Items.Add("No." + cont2 + 1 + "Alumno: " + nombre + " Taller: " + nombreTaller);
+
+
+                writer.Write(nombrePaciente);
+                writer.Write(" tiene cita con el doctor ");
+                writer.Write(nombreDoctor);
+                writer.Write(" el ");
+                writer.Write(fecha);
+                writer.Write(" a las ");
+                writer.WriteLine(hora);
+
+              
                 // cont2++;
 
+                
 
 
             }
@@ -83,6 +110,95 @@ namespace parcial1
         private void button1_Click(object sender, EventArgs e)
         {
             GuardarCita(@"C:\Users\diegoruiz\source\repos\parcial1\parcial1\bin\Debug\Citas.txt");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            leerCitas();
+        }
+        private void leerCitasOrdenadas()
+        {
+            string nombreArchivo = @"C:\Users\diegoruiz\source\repos\parcial1\parcial1\bin\Debug\Citas.txt";
+            FileStream stream = new FileStream(nombreArchivo, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            List<string> citas = new List<string>();
+
+            while (reader.Peek() > -1)
+            {
+                string linea = reader.ReadLine();
+                citas.Add(linea);
+            }
+            reader.Close();
+
+            // Ordenar las citas por fecha más reciente
+            citas = citas.OrderByDescending(cita =>
+            {
+                string[] partes = cita.Split(' ');
+                string fecha = partes[5]; // Asumiendo que la fecha está en la posición 5
+                DateTime fechaCita;
+                DateTime.TryParse(fecha, out fechaCita);
+                return fechaCita;
+            }).ToList();
+
+            mostrar.Items.Clear();
+            Itemas.Items.Clear();
+
+            foreach (string cita in citas)
+            {
+                mostrar.Items.Add(cita);
+                Itemas.Items.Add(cita);
+            }
+
+            label1.Text = "Cantidad de Citas: " + citas.Count;
+        }
+
+        private void leerCitasOrdenadasPorDoctor()
+        {
+            string nombreArchivo = @"C:\Users\diegoruiz\source\repos\parcial1\parcial1\bin\Debug\Citas.txt";
+            FileStream stream = new FileStream(nombreArchivo, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            List<string> citas = new List<string>();
+
+            while (reader.Peek() > -1)
+            {
+                string linea = reader.ReadLine();
+                citas.Add(linea);
+            }
+            reader.Close();
+
+            // Ordenar las citas por el nombre del doctor  
+            citas = citas.OrderBy(cita =>
+            {
+                string[] partes = cita.Split(' ');
+                string nombreDoctor = partes[4]; // Asumiendo que el nombre del doctor está en la posición 4  
+                return nombreDoctor;
+            }).ToList();
+
+            mostrar.Items.Clear();
+            Itemas.Items.Clear();
+
+            foreach (string cita in citas)
+            {
+                mostrar.Items.Add(cita);
+                Itemas.Items.Add(cita);
+            }
+
+            label1.Text = "Cantidad de Citas: " + citas.Count;
+        }
+          
+        private void button3_Click(object sender, EventArgs e)
+        {
+            leerCitasOrdenadas();
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+  leerCitasOrdenadasPorDoctor();
         }
     }
 }
